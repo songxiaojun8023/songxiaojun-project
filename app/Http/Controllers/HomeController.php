2ae688e62287da71a9960c7e615e51c35a838a82
songxiaojun <1432677572.com>
2019-05-28 23:42:23 +0800

    创建了所有业务所需要的接口，路由，及模版页面

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -23,6 +23,54 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        //支撑后台首页的数据 [ ‘question’=>[xxx10条]，’questionAndAnswer’=>[xxx10条] ]；
+
+        $data['question']=[
+                            ['question_id'=>1, 'question'=>'who are you ?', 'status'=>1, 'answer_id'=>'', 'uid'=>1, 'created_at'=>'1525251'],
+                            ['question_id'=>2, 'question'=>'who 2 ?', 'status'=>1, 'answer_id'=>'', 'uid'=>1, 'created_at'=>'1525251'],
+                            ['question_id'=>3, 'question'=>'who 3 ?', 'status'=>1, 'answer_id'=>'', 'uid'=>1, 'created_at'=>'1525251'],
+        ];
+        $data['questionAnswer']=[
+                                [
+                                'question_id'=>1,
+                                'question'=>'who ?',
+                                'status'=>1,
+                                'answer_id'=>'12,110,3',
+                                'uid'=>1,
+                                'created_at'=>'1525251',
+                                'answerList'=>[
+                                        ['answer_id'=>12,'answer'=>'you goods !','question_id'=>1,'uid'=>1,'collect_num'=>3,'status'=>1,'created_at'=>'15232323'],
+                                        ['answer_id'=>110,'answer'=>'no good !','question_id'=>2,'uid'=>1,'collect_num'=>0,'status'=>1,'created_at'=>'1523239'],
+                                        ['answer_id'=>3,'answer'=>'very good !','question_id'=>3,'uid'=>1,'collect_num'=>13,'status'=>1,'created_at'=>'15232323'],
+
+                                ]],
+                                [
+                                    'question_id'=>1,
+                                    'question'=>'who ?',
+                                    'status'=>1,
+                                    'answer_id'=>'12,110,3',
+                                    'uid'=>1,
+                                    'created_at'=>'1525251',
+                                    'answerList'=>[
+                                        ['answer_id'=>12,'answer'=>'you goods !','question_id'=>1,'uid'=>1,'collect_num'=>3,'status'=>1,'created_at'=>'15232323'],
+                                        ['answer_id'=>110,'answer'=>'no good !','question_id'=>2,'uid'=>1,'collect_num'=>0,'status'=>1,'created_at'=>'1523239'],
+                                        ['answer_id'=>3,'answer'=>'very good !','question_id'=>3,'uid'=>1,'collect_num'=>13,'status'=>1,'created_at'=>'15232323'],
+                                ]],
+                                [
+                                    'question_id'=>1,
+                                    'question'=>'who ?',
+                                    'status'=>1,
+                                    'answer_id'=>'12,110,3',
+                                    'uid'=>1,
+                                    'created_at'=>'1525251',
+                                    'answerList'=>[
+                                        ['answer_id'=>12,'answer'=>'you goods !','question_id'=>1,'uid'=>1,'collect_num'=>3,'status'=>1,'created_at'=>'15232323'],
+                                        ['answer_id'=>110,'answer'=>'no good !','question_id'=>2,'uid'=>1,'collect_num'=>0,'status'=>1,'created_at'=>'1523239'],
+                                        ['answer_id'=>3,'answer'=>'very good !','question_id'=>3,'uid'=>1,'collect_num'=>13,'status'=>1,'created_at'=>'15232323'],
+
+                                ]]
+        ];
+
+        return view('home',$data);
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -15,6 +15,7 @@
                     @endif
 
                     You are logged in!
+                    {{$question[0]['question']}}
                 </div>
             </div>
         </div>
