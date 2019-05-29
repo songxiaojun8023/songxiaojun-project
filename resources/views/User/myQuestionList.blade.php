5225f16490cebb37d10eecef5111c513adfb4cf1
guopenghang <1658204059@qq.com>
2019-05-29 17:14:10 +0800

    郭鹏航2019/5/29 下午提交

--- resources/views/User/myQuestionList.blade.php
+++ resources/views/User/myQuestionList.blade.php
@@ -1,2 +1,11 @@
-<?php
-        echo '我的问题列表 页面';
+@extends('layouts.app')
+    {{--我的问题--}}
+@section('content')
+    <link rel="stylesheet" href="../layui/css/layui.css">
+
+    <div align="center">
+        <a href="/question/showOneQuestion" class="layui-btnMy layui-btn-fluidMy">问题详情页</a>
+    </div>
+
+@endsection
+<script type="text/javascript" src="../layui/layui.js"></script>
