b3d6974439832e261bb7e30fa1e692bdc05b53db
王畅 <1752914255@qq.com>
2019-05-29 17:09:05 +0800

    首页模拟数据

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,24 +1,46 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                    {{$question[0]['question']}}
-                </div>
+<div class="right">
+    <div type="search" align="center" class="top">
+        <input type="text" style="width: 40%;height: 35px">
+        <button class="layui-btn layui-btn-radius layui-btn-normal">搜索<tton>
+    </div>
+    <div class="center">
+        <div class="r_left">
+            <div class="title">
+               <h3>最新有答案列表</h3> 
+            </div>
+            <div class="more">
+                <a href="">更多>></a>
+            </div>
+            <div class="content">
+             <ul>
+                @foreach($questionAnswer as $k=>$v)
+                <li><p align="center"><b><font size="5px"><a href="">{{$questionAnswer[$k]['question']}}</a></font></b></p></li>
+                @foreach($questionAnswer[$k]['answerList'] as $kk=>$vv)
+                <li><p align="center">{{$questionAnswer[$k]['answerList'][$kk]['answer']}}</p></li>
+                @endforeach
+                @endforeach
+             </ul>   
             </div>
         </div>
+        <div class="r_right">
+            <div class="title">
+               <h3>最新无答案列表</h3> 
+            </div>
+            <div class="more">
+                <a href="">更多>></a>
+            </div>
+            <div class="content">
+             <ul>
+                @foreach($question as $k=>$v)
+                <li><p align="center"><b><font size="5px"><a href="">{{$questionAnswer[$k]['question']}}</a></font></b></p></li>
+                @endforeach
+             </ul>   
+            </div>
+        </div> 
     </div>
 </div>
+<div class="clear"></div>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,41 +17,126 @@
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
 
     <!-- Styles -->
+    <link rel="stylesheet" href="./layui/css/layui.css">
+    <script src="./layui/layui.js"></script>
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <style>
+        *{
+            margin:0;
+            padding:0;
+        }
+        .left{
+            float:left;
+           
+            width:200px;
+            height:544.5px;
+            background:#4E5465;
+        }
+        .nav{
+            width:100%;
+            height:56px;
+        }
+        .bottom{
+           
+            width:100%;
+            height:544px; 
+        }
+        .layui-nav{
+            margin-bottom:0rem;
+
+        }
+        .nav-right{
+            float:right;
+            line-height:45px;
+
+        }
+         .right{
+                width:1149px;
+                height:543px;
+                float:right;
+                
+         }
+         .top{
+           margin:8px auto;
+
+         }
+         .clear{
+            clear:both;
+         }
+         .center{
+           
+            width:1120px;
+            height:488px;
+            margin-left:10px;
+            margin-right:10px;
+         }
+         .r_left{
+           border-right:1px solid black;
+            width:545px;
+            height:470px;
+            margin-left:7px;
+            margin-top:10px; 
+            float:left;
+         }
+         .r_right{
+           
+            width:540px;
+            height:470px;
+            margin-right:7px;
+            margin-top:10px; 
+            float:right;
+         }
+         .title{
+           
+            width:180px;
+            height:37px;
+            margin:2px auto;
+         }
+         .more{
+           
+            width:50px;
+            height:23px;
+            float:right;
+         }
+         .content{
+           
+            width:520px;
+            height:400px;
+            margin-top:28px;
+            margin-left:5px;
+            margin-right:5px;
+         }
+        .content li{
+            
+            width:520px;
+            height:28px;
+            margin-top:5px;
+        }
+</style>
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
+        <ul class="layui-nav layui-bg-green" lay-filter="">
+          <li class="layui-nav-item">
+           <a class="navbar-brand" href="{{ url('/') }}">
                     {{ config('app.name', 'Laravel') }}
                 </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
-
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            <li class="nav-item">
+          </li>
+          
+                         @guest
+                            <li class="nav-right">
                                 <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
                             </li>
                             @if (Route::has('register'))
-                                <li class="nav-item">
+                                <li class="nav-right">
                                     <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
                                 </li>
                             @endif
                         @else
-                            <li class="nav-item dropdown">
+                            <li class="nav-right">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
+                                    
+                                    <font color="black">{{ Auth::user()->name }}</font><span class="caret" ></span>
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
@@ -67,12 +152,24 @@
                                 </div>
                             </li>
                         @endguest
-                    </ul>
-                </div>
-            </div>
-        </nav>
+        </ul>
 
-        <main class="py-4">
+        <main class="bottom">
+            <div class="left">
+
+            <ul class="layui-nav layui-nav-tree" lay-filter="test">
+            <!-- 侧边导航: <ul class="layui-nav layui-nav-tree layui-nav-side"> -->
+              <li class="layui-nav-item">
+                <a href="user/myCenter">用户中心</a>
+              </li>
+              <li class="layui-nav-item">
+                <a href="question/pushQuestion">发布问题</a>
+              </li>
+              <li class="layui-nav-item">
+                <a href="test/startTest">模拟答题</a>
+              </li>
+            </ul>
+            </div>
             @yield('content')
         </main>
     </div>
