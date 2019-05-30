49f1e65dc52af749c98a74dfd7865382d50e405f
songxiaojun <1432677572.com>
2019-05-30 23:31:32 +0800

    修改 王畅删除的question控制器，因为电脑问题，question内容被删没了

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -14,29 +14,39 @@ class QuestionController extends Controller
         return view('question.showQuestion');
     }
 
-    //显示指定某个问题详情
+    //显示指定某个问题详情---问题，多个答案，作者，收藏，采纳
     public function showOneQuestion(){
-
-        return view('question.showOneQuestion');
+        $question = new Question();
+        $data = $question->showOneQuestion();
+        return view('question.showOneQuestion',$data)->with('data',$data);
     }
 
-    //最新有答案问题列表
-    public function moreQuestionAnswer(){
-
-        return  view('question.moreQuestionAnswer');
+    //有答案问题列表
+    public function questionAnswerList(){
+        $question = new Question();
+        $data=$question->questionAnswerList();//return $data;
+        return  view('question.questionAnswerList')->with('data',json_encode($data));
     }
 
-    //最新有答案问题列表
-    public function moreQuestionList(){
+    //无答案问题列表
+    public function showQuestionList(){
+        $question = new Question();
+        $data = $question->showQuestionList();//return $data;
+        return  view('question.showQuestionList')->with('data',json_encode($data));
 
-        return  view('question.moreQuestionList');
     }
 
-    //发布问题
+    //无答案问题详情页,一条问题
+    public function showNoAnswerQuestion(){
+
+        $question = new Question();
+        $data = $question->showNoAnswerQuestion();
+        return  view('question.showNoAnswerQuestion')->with('data',$data);
+    }
+
+    //发布问题--页面
     public function pushQuestion(){
-
         return  view('question.pushQuestion');
-
     }
 
     //图片识别
@@ -54,6 +64,8 @@ class QuestionController extends Controller
     //form表单提交 录入试题
     public function pushFormQuestion(){
 
+        $question = new Question();
+        $question->addAnswer();
         return ['code'=>200,'message'=>'ok'];
     }
 
