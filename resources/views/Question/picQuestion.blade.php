e3fe11c5be3ded7dbd9783c2dae4d250eff4a9c0
王畅 <1752914255@qq.com>
2019-05-31 15:43:08 +0800

    修改后的picQuestion.blade.php和pushQuestion.blade.php

--- resources/views/Question/picQuestion.blade.php
+++ resources/views/Question/picQuestion.blade.php
@@ -1,5 +1,33 @@
 @extends('layout')
 @section('title', 'test')
 @section('content')
-    这是图片识别后的结果。。
+    <div style="margin-left:42%;color:#FF1493;">
+        <h2>图片识别出来的结果</h2>
+
+    </div>
+    <div style="border-right:1px solid #8B008B;width:23%;height:80%;margin-top:5%">
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+
+    </div>
 @endsection
