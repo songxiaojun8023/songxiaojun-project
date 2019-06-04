343f37dcb20d067ca251b80000070ca180971748
songxiaojun <1432677572.com>
2019-06-04 09:19:02 +0800

    图片

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -31,20 +31,7 @@ class HomeController extends Controller
         $data['question']=$question->getQuestionLists();
         //有答案类表
         $data['questionAnswer']=$question->getQuestionAnswerList();
-<<<<<<< HEAD
-        //dd($data['questionAnswer']);
-        //echo count($data["questionAnswer"]);die;
-//            for($i=0;$i<count($data["questionAnswer"]);$i++){
-//                echo $i;
-//                echo $data["questionAnswer"][$i]['question']."<br>";
-//                echo $data['questionAnswer'][$i]['answerList']['answer']."<br>";
-//            }
-//
-//
-//        die;
-        //return $data;
-=======
->>>>>>> guopenghang
+
         return view('home',$data);
     }
 }
